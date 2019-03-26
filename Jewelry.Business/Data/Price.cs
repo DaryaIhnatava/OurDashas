00d563b4b52549b3ce622d8c7748d6f26f124c93
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-03-26 10:45:50 +0300

    Price was changed with documentation

--- Jewelry.Business/Data/Price.cs
+++ Jewelry.Business/Data/Price.cs
@@ -1,21 +1,64 @@
-﻿namespace Jewelry.Business.Data
+﻿// <copyright file="Price.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+namespace Jewelry.Business.Data
 {
     #region Usings
     using System;
     #endregion
 
+    /// <summary>
+    /// Enumeration currency
+    /// </summary>
+    public enum Currency
+    {
+        /// <summary>
+        /// Dollar value
+        /// </summary>
+        Dollar,
+
+        /// <summary>
+        /// Ruble value
+        /// </summary>
+        Ruble,
+
+        /// <summary>
+        /// Euro value
+        /// </summary>
+        Euro
+    }
+
+    /// <summary>
+    /// Price class
+    /// </summary>
     public class Price : IComparable
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Price" /> class
+        /// </summary>
+        /// <param name="value">value of price</param>
+        /// <param name="currency">value of price currency</param>
         public Price(double value, Currency currency)
         {
-            Value = value;
-            Currency = currency;
+            this.Value = value;
+            this.Currency = currency;
         }
 
+        /// <summary>
+        /// Gets and sets value
+        /// </summary>
         public double Value { get; set; }
 
+        /// <summary>
+        /// Gets and sets currency
+        /// </summary>
         public Currency Currency { get; set; }
 
+        /// <summary>
+        /// Method for compare of jewelries
+        /// </summary>
+        /// <param name="obj">object for compare</param>
+        /// <returns> compared prices</returns>
         public int CompareTo(object obj)
         {
             if (obj is Price price)
@@ -25,11 +68,5 @@
 
             throw new ArgumentException("Parameter is not a Price!");
         }
-    }
-    public enum Currency
-    {
-        Dollar,
-        Ruble,
-        Euro
     }
 }
