f843a032c3917096e7934393b37986fa4a217adf
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-03-26 12:16:30 +0300

    renamig and adding documentation to ijewelryRepository

---
+++ Jewelry.Database/JewellryRepository/IJewelryRepository.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IJewelryRepository.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+namespace Jewelry.Database.JewelryRepository
+{
+    #region Usings
+    using System.Collections.Generic;
+    using Jewelry.Database.Data;
+    #endregion
+
+    /// <summary>
+    /// Interface of jewelry repository
+    /// </summary>
+    public interface IJewelryRepository
+    {
+        /// <summary>
+        /// Gets the jewelries.
+        /// </summary>
+        /// <returns>Enumeration of jewelries</returns>
+        IEnumerable<Jewelry> GetJewelries();
+    }
+}
