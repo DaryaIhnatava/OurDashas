20578a91df7f81183445346ccbc80fb7f6499c24
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-03-26 12:18:25 +0300

    Documentation to price class was added

--- Jewelry.Database/Data/Price.cs
+++ Jewelry.Database/Data/Price.cs
@@ -1,32 +1,74 @@
-﻿namespace Jewelry.Database.Data
+﻿// <copyright file="Price.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+namespace Jewelry.Database.Data
 {
+    /// <summary>
+    /// Currency enumeration
+    /// </summary>
+    public enum Currency
+    {
+        /// <summary>
+        /// The dollar
+        /// </summary>
+        Dollar,
+
+        /// <summary>
+        /// The ruble
+        /// </summary>
+        Ruble,
+
+        /// <summary>
+        /// The euro
+        /// </summary>
+        Euro
+    }
+
+    /// <summary>
+    /// Price class
+    /// </summary>
     public class Price
     {
-        public Price(double value, Currency currency)
-        {
-            Value = value;
-            Currency = currency;
-        }
-
+        /// <summary>
+        /// The value
+        /// </summary>
         private double value;
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Price"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="currency">The currency.</param>
+        public Price(double value, Currency currency)
+        {
+            this.Value = value;
+            this.Currency = currency;
+        }
+        
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
         public double Value
         {
-            get => value;
+            get => this.value;
             set
             {
                 if (value > 0)
+                {
                     this.value = value;
+                }
             }
         }
-        public Currency Currency { get; set; }
-    }
 
-    public enum Currency
-    {
-        Dollar,
-        Ruble,
-        Euro
+        /// <summary>
+        /// Gets or sets the currency.
+        /// </summary>
+        /// <value>
+        /// The currency.
+        /// </value>
+        public Currency Currency { get; set; }
     }
 }
