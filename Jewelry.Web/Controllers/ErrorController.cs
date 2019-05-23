5f6e7e20456337773d34e1e0f491df52647129a2
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:32:02 +0300

    Some new methods for new pages were added to error controller

--- Jewelry.Web/Controllers/ErrorController.cs
+++ Jewelry.Web/Controllers/ErrorController.cs
@@ -4,6 +4,12 @@
 namespace Jewelry.Web.Controllers
 {
     #region Usings
+    using System;
+
+    using Jewelry.Web.Models;
+
+    using Microsoft.AspNetCore.Diagnostics;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     #endregion
 
@@ -13,6 +19,30 @@ namespace Jewelry.Web.Controllers
     /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
     public class ErrorController : Controller
     {
+        /// <summary>
+        /// StatusCode method
+        /// </summary>
+        /// <returns>view with status code page</returns>
+        public IActionResult StatusCode()
+        {
+            var exceptionHandlerPathFeature =
+                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            var path = exceptionHandlerPathFeature?.Path;
+            var error = exceptionHandlerPathFeature?.Error;
+            var code = HttpContext.Response?.StatusCode;
+            if (code != null)
+            {
+                switch (code)
+                {
+                    case 500: return this.RedirectToAction("StatusCode500");
+                    case 403: return this.RedirectToAction("StatusCode404");
+                        default: return this.RedirectToAction("OopsPage");
+                }
+            }
+
+            return this.View();
+        }
+
         /// <summary>
         /// Statuses the code404.
         /// </summary>
@@ -38,6 +68,46 @@ namespace Jewelry.Web.Controllers
         public IActionResult StatusCode403()
         {
             return this.View();
+        }
+
+        /// <summary>
+        /// OopsPage
+        /// </summary>
+        /// <returns>View page</returns>
+        public IActionResult OopsPage()
+        {
+            return this.View();
+        }
+
+        public IActionResult ForDeveloper()
+        {
+            var exceptionHandler = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            if (exceptionHandler?.Error != null)
+            {
+                var exceptionModel = this.ReturnViewModel(exceptionHandler?.Error, HttpContext);
+                return this.View(exceptionModel);
+            }
+
+            return this.Redirect("/Jewelry/Index");
+        }
+
+        /// <summary>
+        /// ReturnViewModel method
+        /// </summary>
+        /// <param name="exception">exception</param>
+        /// <param name="context">context</param>
+        /// <returns>Error view model</returns>
+        private ErrorViewModel ReturnViewModel(Exception exception, HttpContext context)
+        {
+            return new ErrorViewModel()
+            {
+                Message = exception.Message,
+                InnerException = exception.InnerException,
+                Source = exception.Source,
+                StatusCode = context.Response.StatusCode,
+                HelpLink = exception.HelpLink,
+                StackTrace = exception.StackTrace
+            };
         }
     }
 }
