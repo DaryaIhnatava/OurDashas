4cd57f1668af080a252549dc81f24a2eab30b3f4
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:33:49 +0300

    Category service for getting all categories for jewelry filter was added

---
+++ Jewelry.Business/CategoryService/CategoryService.cs
@@ -0,0 +1,90 @@
+﻿// <copyright file="CategoryService.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+namespace Jewelry.Business.CategoryService
+{
+    using System;
+    #region Usings
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Jewelry.Business.Data;
+    using Jewelry.Business.i18n;
+    using Jewelry.Business.JewelryService;
+    #endregion
+
+    /// <summary>
+    /// Category service
+    /// </summary>
+    public class CategoryService : ICategoryService
+    {
+        /// <summary>
+        /// The jewelry service
+        /// </summary>
+        private readonly IJewelryService jewelryService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoryService"/> class.
+        /// </summary>
+        /// <param name="jewelryService">The jewelry service.</param>
+        public CategoryService(IJewelryService jewelryService)
+        {
+            this.jewelryService = jewelryService;
+        }
+
+        /// <summary>
+        /// Gets the categories.
+        /// </summary>
+        /// <returns>Enumerable of categories</returns>
+        public IEnumerable<Category> GetCategories()
+        {
+            IEnumerable<Data.Jewelry> jewelries = this.jewelryService.GetJewelries();
+            List<Category> categories = new List<Category>();
+
+            foreach (var property in typeof(Data.Jewelry).GetProperties().Where(q => q.Name != "Price" && q.Name != "Id"))
+            {
+                Type myType = typeof(Resource);
+                PropertyInfo[] properties = myType.GetProperties(
+                       BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+                var t = properties.Where(x => x.Name == property.Name).FirstOrDefault()?.GetValue(myType, null).ToString();
+                categories.Add(new Category()
+                {
+                    PropertyName = properties.Where(x => x.Name == property.Name).FirstOrDefault()?.GetValue(myType, null).ToString(),
+                    PropertyValues = new List<Value>()
+                });
+                var values = jewelries.Select(j => property.GetValue(j).ToString()).Distinct().ToList();
+                foreach (var item in values)
+                {
+                    categories[categories.Count - 1].PropertyValues.Add(
+                        new Value()
+                        {
+                            PropertyValue = item,
+                            Checked = false
+                        });
+                }
+            }
+
+            categories.Add(
+                    new Category()
+                    {
+                        PropertyName = Resource.Price,
+                        PropertyValues = new List<Value>()
+                        {
+                            new Value()
+                            {
+                                PropertyValue = jewelries.Min(w => w.Price.Value).ToString(),
+                                Checked = false
+                            },
+
+                            new Value()
+                            {
+                                PropertyValue = jewelries.Max(w => w.Price.Value).ToString(),
+                                Checked = false
+                            }
+                        }
+                    });
+
+            return categories;
+        }
+    }
+}
---
+++ Jewelry.Business/CategoryService/ICategoryService.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="ICategoryService.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+namespace Jewelry.Business.CategoryService
+{
+    #region Usings
+    using System.Collections.Generic;
+
+    using Jewelry.Business.Data;
+    #endregion
+
+    /// <summary>
+    /// Interface of category service
+    /// </summary>
+    public interface ICategoryService
+    {
+        /// <summary>
+        /// Get categories
+        /// </summary>
+        /// <returns>enumerable of categories</returns>
+        IEnumerable<Category> GetCategories();
+    }
+}
