ea5f5d7d0bb9f836bc0c045bf08a9b8fcb9715da
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:42:49 +0300

    JewelryService was changed with using of cache service

--- Jewelry.Business/JewelryService/JewelryService.cs
+++ Jewelry.Business/JewelryService/JewelryService.cs
@@ -1,15 +1,20 @@
 ﻿// <copyright file="JewelryService.cs" company="CompanyName">
 //     Company copyright tag.
 // </copyright>
+
+using Jewelry.Business.CacheService;
+
 namespace Jewelry.Business.JewelryService
 {
     #region Usings
     using System;
     using System.Collections.Generic;
     using System.Linq;
+
     using Jewelry.Business.Data;
     using Jewelry.Business.FilterService;
     using Jewelry.Database.JewelryRepository;
+    using Microsoft.Extensions.Configuration;
     #endregion
 
     /// <summary>
@@ -22,25 +27,56 @@ namespace Jewelry.Business.JewelryService
         /// </summary>
         private readonly IJewelryRepository jewelryRepository;
 
+        private readonly ICacheService cacheService;
+
+        private readonly IConfiguration configuration;
+
+        private readonly CacheSettings cacheSettings;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="JewelryService"/> class.
         /// </summary>
         /// <param name="jewelryRepository">The Jewelry repository.</param>
-        public JewelryService(IJewelryRepository jewelryRepository)
+        public JewelryService(IJewelryRepository jewelryRepository, ICacheService cacheService, IConfiguration configuration)
         {
             this.jewelryRepository = jewelryRepository;
+            this.cacheService = cacheService;
+            this.configuration = configuration;
+            cacheSettings = new CacheSettings();
+            configuration.GetSection("Cache").Bind(cacheSettings);
         }
 
         /// <summary>
         /// Gets the jewelries.
         /// </summary>
         /// <returns>All jewelries</returns>
-        public IEnumerable<Data.Jewelry> GetJewelries()
+        public IEnumerable<Business.Data.Jewelry> GetJewelries()
+        {
+            if (cacheSettings.IsEnable)
+            {
+                var jewelriesFromCache = cacheService.GetFromCache();
+                if (jewelriesFromCache!=null && jewelriesFromCache.Any())
+                {
+                    return jewelriesFromCache;
+                }
+            }
+            var jewelries = GetJewelriesFromRepository();
+            if (cacheSettings.IsEnable)
+            {
+                cacheService.AddToCache(jewelries);
+            }
+            return jewelries;
+        }
+        /// <summary>
+        /// Gets the jewelries from repository.
+        /// </summary>
+        /// <returns>IEnumerable of Jewelries</returns>
+        public IEnumerable<Business.Data.Jewelry> GetJewelriesFromRepository()
         {
             var jewelries = this.jewelryRepository.GetJewelries();
             foreach (var jewelry in jewelries)
             {
-                yield return new Business.Data.Jewelry(jewelry.Shape, jewelry.Metal, jewelry.Brand, jewelry.Price.Value, (Currency)jewelry.Price.Currency);
+                yield return new Business.Data.Jewelry(jewelry.Id,jewelry.Shape, jewelry.Metal, jewelry.Brand, jewelry.Price.Value, (Currency)jewelry.Price.Currency);
             }
         }
 
@@ -52,6 +88,11 @@ namespace Jewelry.Business.JewelryService
         public IEnumerable<Data.Jewelry> GetSortedJewelriesByPropertyName(string propertyName)
         {
             var jewelries = this.GetJewelries();
+            if (propertyName == null)
+            {
+                return jewelries;
+            }
+
             var sortedJewelries = jewelries.OrderByPropertyName(propertyName);
             return sortedJewelries;
         }
@@ -81,6 +122,11 @@ namespace Jewelry.Business.JewelryService
         /// <returns>New jewelry price of jewelry</returns>
         public Price ConvertPriceCurrency(Price jewelryPrice, Currency currency)
         {
+            if (jewelryPrice == null)
+            {
+                throw new ArgumentException();
+            }
+
             if (jewelryPrice.Currency == currency)
             {
                 return jewelryPrice;
