650330fda732764cffd61015e34d87bf7635af80
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:37:26 +0300

    New hnibernate helper was added

---
+++ Jewelry.Database/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace Jewelry.Database.Nhibernate
+{
+    using System.Reflection;
+    using NHibernate;
+    using NHibernate.Cfg;
+    using NHibernate.Dialect;
+    using NHibernate.Driver;
+    using NHibernate.Mapping.Attributes;
+
+    public class NHibernateHelper
+    {
+        public static NHibernate.ISession OpenSession()
+        {
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(x => 
+            {
+                x.ConnectionString = @"Data Source=EPBYMOGW0027\SQLEXPRESS;Initial Catalog=Jewelry;User ID=JUser;Password=Qwerty123;";
+                //x.ConnectionString = @"Data Source=LAPTOP-868QL38T\SQLEXPRESS;Initial Catalog=training;User ID=JUser;Password=qwerty123;"; 
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2008Dialect>();
+                x.LogSqlInConsole = true;
+            });
+            HbmSerializer.Default.Validate = true;
+            var stream = HbmSerializer.Default.Serialize(Assembly.GetExecutingAssembly());
+            cfg.AddInputStream(stream);
+            ISessionFactory sessionFactory = cfg.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
