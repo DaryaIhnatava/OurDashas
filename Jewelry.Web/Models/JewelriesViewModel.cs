521dbcd66623d772a8972644ff54b40f615cab65
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:36:08 +0300

    Jewelries viewmodel for filter was added

---
+++ Jewelry.Web/Models/JewelriesViewModel.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="JewelriesViewModel.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+namespace Jewelry.Web.Models
+{
+    #region Usings
+    using System.Collections.Generic;
+
+    using Jewelry.Business.Data;
+    #endregion
+
+    /// <summary>
+    /// Model for UI
+    /// </summary>
+    public class JewelriesViewModel
+    {
+        /// <summary>
+        /// Gets Jewelries
+        /// </summary>
+        public IEnumerable<Jewelry> Jewelries { get; set; }
+
+        /// <summary>
+        /// Gets categories of jewelry
+        /// </summary>
+        public CategoryWithPropertiesViewModel categoryWithPropertiesViewModel { get; set; }
+    }
+}
