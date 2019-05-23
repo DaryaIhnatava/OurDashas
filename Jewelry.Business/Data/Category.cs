9db0367d31e5991dd740f257034835e5a09519a9
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:40:11 +0300

    Category and value model were added

---
+++ Jewelry.Business/Data/Category.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="JewelryExtension.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+using System.Resources;
+using Jewelry.Business.i18n;
+
+namespace Jewelry.Business.Data
+{
+    #region Usings
+    using System.Collections.Generic;
+    #endregion
+
+    /// <summary>
+    /// Category class
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Category property
+        /// </summary>
+        public Category()
+        {
+            this.PropertyValues = new List<Value>();
+        }
+
+        /// <summary>
+        /// PropertyName property
+        /// </summary>
+        //[Display(Name  = Resource.PropertyName)]
+        public string PropertyName { get; set; }
+
+        /// <summary>
+        /// PropertyValues property
+        /// </summary>
+        public List<Value> PropertyValues { get; set; }
+    }
+}
---
+++ Jewelry.Business/Data/Value.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Value.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+namespace Jewelry.Business.Data
+{
+    /// <summary>
+    /// Value class
+    /// </summary>
+    public class Value
+    {
+        /// <summary>
+        /// PropertyValue property
+        /// </summary>
+        public string PropertyValue { get; set; }
+
+        /// <summary>
+        /// Checked property
+        /// </summary>
+        public bool Checked { get; set; }
+    }
+}
