4c3804bd22775093e488e87c093175ea1f6d7fd8
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:28:12 +0300

    File for encryption password was added

---
+++ Jewelry.Business/LoginService/Encryption.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Jewelry.Business.LoginService
+{
+    public static class Encryption
+    {
+        public static string GetMd5Hash(string input)
+        {
+            MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+        
+        public static bool VerifyMd5Hash(string input, string hash)
+        {
+            string hashOfInput = GetMd5Hash(input);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
