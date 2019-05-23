91e8974f8a73056dd47845944df4cb6c92846204
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:35:51 +0300

    Localization middleware was added for changing culture of application

---
+++ Jewelry.Web/Middlewares/LocalizationMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Jewelry.Web.Middlewares
+{
+    public class LocalizationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration configuration;
+
+        public LocalizationMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            this._next = next;
+            this.configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var lang = context.Request.Cookies["lang"];
+            if (lang == null)
+            {
+                context.Response.Cookies.Append("lang", configuration.GetSection("Culture").Value);
+                lang = configuration.GetSection("Culture").Value;
+            }
+            try
+            {
+                CultureInfo.CurrentCulture = new CultureInfo(lang);
+                CultureInfo.CurrentUICulture = new CultureInfo(lang);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
