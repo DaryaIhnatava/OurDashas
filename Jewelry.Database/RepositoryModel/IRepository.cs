6bb19f0e2c113f7a2dc1f831ad04502b7a4778ea
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:37:04 +0300

    Hnibarnate repository was added foк data manipulation

---
+++ Jewelry.Database/RepositoryModel/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Jewelry.Database.RepositoryModel
+{
+    using System.Collections.Generic;
+
+    public interface IRepository<T>
+    {
+        T GetById(int Id);
+        void InsertOrUpdate(T item);
+        void Delete(T item);
+        IList<T> GetList();
+
+        IEnumerable<T> ListByProperty(string propertyName, object value);
+    }
+}
---
+++ Jewelry.Database/RepositoryModel/Repository.cs
@@ -0,0 +1,73 @@
+﻿namespace Jewelry.Database.RepositoryModel
+{
+    using System.Collections.Generic;
+    using Jewelry.Database.Nhibernate;
+    using NHibernate;
+    using NHibernate.Criterion;
+
+    public class Repository<T> : IRepository<T>
+    {
+        public T GetById(int Id)
+        {
+            T item;
+            using (var session = GetSession())
+            {
+                item = session.Get<T>(Id);
+            }
+            return item;
+        }
+
+        public void InsertOrUpdate(T item)
+        {
+            using (var session = GetSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.SaveOrUpdate(item);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public void Delete(T item)
+        {
+            using (var session = GetSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Delete(item);
+                    transaction.Commit();
+                }
+            }
+        }
+        public IList<T> GetList()
+        {
+            using (var session = GetSession())
+            {
+                return session
+                    .CreateCriteria(typeof(T))
+                    .List<T>();
+            }
+        }
+
+        public IEnumerable<T> ListByProperty(string propertyName, object value)
+        {
+            using (var session = GetSession())
+            {
+                return session
+                    .CreateCriteria(typeof(T))
+                    .Add(Restrictions.Eq(propertyName, value))
+                    .List<T>();
+            }
+        }
+
+        private ISession GetSession()
+        {
+            return NHibernateHelper.OpenSession();
+        }
+        private void DisposeSession(ISession session)
+        {
+            session.Dispose();
+        }
+    }
+}
