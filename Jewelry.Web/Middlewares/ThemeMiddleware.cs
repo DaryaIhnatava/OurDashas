f8814e85b2b1926ac0f55c9c39def5236ae77901
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:36:37 +0300

    Theme middleware was added with helping of changing view of application

---
+++ Jewelry.Web/Middlewares/ThemeMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Jewelry.Web.Middlewares
+{
+    public class ThemeMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration configuration;
+
+        public ThemeMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            this._next = next;
+            this.configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var theme = context.Request.Cookies["theme"];
+            if (theme == null)
+            {
+                context.Response.Cookies.Append("theme", configuration.GetSection("Theme").Value);
+                theme = configuration.GetSection("Theme").Value;
+            }
+            await _next.Invoke(context);
+        }
+    }
+}
