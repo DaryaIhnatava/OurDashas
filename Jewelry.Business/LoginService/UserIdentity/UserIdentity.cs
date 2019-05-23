1044e7eafdf5f370ea49d0292e2b03f7ebdb1335
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:43:08 +0300

    UserIdentity provider was added

---
+++ Jewelry.Business/LoginService/UserIdentity/UserIdentity.cs
@@ -0,0 +1,53 @@
+﻿namespace Jewelry.Business.LoginService.UserIdentity
+{
+    using System.Linq;
+    using System.Security.Principal;
+    using Jewelry.Business.LoginService.Models;
+
+    public class UserIdentity : IIdentity
+    {
+        private readonly IService<User> userRepository;
+
+        public UserIdentity(IService<User> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public User User { get; set; }
+
+        public string AuthenticationType
+        {
+            get
+            {
+                return typeof(User).ToString();
+            }
+        }
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return User != null;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                if (User != null)
+                    return User.Email;
+                return "anonym";
+            }
+
+        }
+
+        public void Init(string login)
+        {
+            if (!string.IsNullOrEmpty(login))
+            {
+                User =(User)userRepository.ListByProperty("Email", login).FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ Jewelry.Business/LoginService/UserProvider/UserProvider.cs
@@ -0,0 +1,32 @@
+﻿namespace Jewelry.Business.LoginService.UserProvider
+{
+    using System.Security.Principal;
+    using Jewelry.Business.LoginService.Models;
+    using Jewelry.Business.LoginService.UserIdentity;
+
+    public class UserProvider:IPrincipal
+    {
+        private readonly IService<User> userService;
+        public UserProvider(IService<User> userService, User user)
+        {
+            this.userService = userService;
+
+            userIndentity = new UserIdentity(userService);
+            userIndentity.User = user;
+        }
+
+        private UserIdentity userIndentity { get; set; }
+
+        public IIdentity Identity
+        {
+            get
+            {
+                return userIndentity;
+            }
+        }
+        public bool IsInRole(string role)
+        {
+            return true;
+        }
+    }
+}
