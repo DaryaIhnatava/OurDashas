73d65ce0a9bbf3a188b57d3cc1e46dacaa59c697
Darya Ihnatava <Darya_Ihnatava@epam.com>
2019-05-23 09:37:39 +0300

    New view models for views were added

---
+++ Jewelry.Web/Models/CategoryWithPropertiesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jewelry.Business.Data;
+
+namespace Jewelry.Web.Models
+{
+    public class CategoryWithPropertiesViewModel
+    {
+        public IDictionary<string, bool> Properties { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
---
+++ Jewelry.Web/Models/JewelriesWithPropertiesListViewModel.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="JewelriesWithPropertiesListViewModel.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+namespace Jewelry.Web.Models
+{
+    #region Usings
+    using System.Collections.Generic;
+    #endregion
+
+    /// <summary>
+    /// Jewelries With Properties List View Model
+    /// </summary>
+    public class JewelriesWithPropertiesListViewModel
+    {
+        /// <summary>
+        /// Gets or sets the Jewelries.
+        /// </summary>
+        /// <value>
+        /// The Jewelries.
+        /// </value>
+        public IEnumerable<Jewelry.Business.Data.Jewelry> Jewelries { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Properties.
+        /// </summary>
+        /// <value>
+        /// The Properties.
+        /// </value>
+        public IEnumerable<string> Properties { get; set; }
+    }
+}
